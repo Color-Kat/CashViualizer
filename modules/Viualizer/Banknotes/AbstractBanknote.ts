3f6f1bcb81bfd0fd23bb846015516d8c44ee2852
Color-Kat <katik7979@gmail.com>
2024-06-23 14:20:43 +0300

    AbstractBanknote.ts

--- modules/Viualizer/Banknotes/AbstractBanknote.ts
+++ modules/Viualizer/Banknotes/AbstractBanknote.ts
@@ -1,3 +1,13 @@
 export abstract class AbstractBanknote {
+    abstract currency: CurrencyType;
+    abstract denomination : number;
 
+    abstract image : string;
+
+    // Сentimeters
+    abstract realWidth: number;
+    abstract realHeight: number;
+    abstract realThickness: number;
+
+    abstract weight: number; // Grams
 }
